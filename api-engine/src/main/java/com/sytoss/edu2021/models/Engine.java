f3ab9b823e9bd57a2d3493377283748491b61405
Nelson <kapitannelson.wf@gmail.com>
2021-06-04 15:09:31 +0300

    New realization of api-cabin

--- api-engine/src/main/java/com/sytoss/edu2021/models/Engine.java
+++ api-engine/src/main/java/com/sytoss/edu2021/models/Engine.java
@@ -1,4 +1,30 @@
+
 package com.sytoss.edu2021.models;
 
+
 public class Engine {
+    private Cabin cabin;
+
+    public void moveUp(int distance) {
+
+    }
+
+    public void moveDown(int distance) {
+
+    }
+
+    //TODO: private??
+    public void start() {
+        cabin.closeDoor();
+        cabin.setMovingState(true);
+        // Realization elevator moving
+        //
+
+    }
+
+    public void stop() {
+        // Realization elevator stop
+        cabin.setMovingState(false);
+        cabin.openDoor();
+    }
 }

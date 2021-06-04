f68dfb67a8eb0f065d2e104e3edb3b9ab5deedd9
Nelson <kapitannelson.wf@gmail.com>
2021-06-04 15:15:08 +0300

    Update api-engine by adding functions set start/end points of route

--- api-engine/src/main/java/com/sytoss/edu2021/models/Engine.java
+++ api-engine/src/main/java/com/sytoss/edu2021/models/Engine.java
@@ -3,7 +3,6 @@ package com.sytoss.edu2021.models;
 
 
 public class Engine {
-    private Cabin cabin;
 
     public void moveUp(int distance) {
 
@@ -13,18 +12,10 @@ public class Engine {
 
     }
 
-    //TODO: private??
     public void start() {
-        cabin.closeDoor();
-        cabin.setMovingState(true);
-        // Realization elevator moving
-        //
-
     }
 
     public void stop() {
-        // Realization elevator stop
-        cabin.setMovingState(false);
-        cabin.openDoor();
+
     }
 }

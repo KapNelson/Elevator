732597d80306c017e7d446102f8808aeb122c7ad
DarkMinimum <danylo.hrechyshkin@nure.ua>
2021-06-07 13:39:53 +0300

    Changed cabin to POST method and added css style

--- api-cabin/src/main/java/com/sytoss/edu2021/controllers/CabinController.java
+++ api-cabin/src/main/java/com/sytoss/edu2021/controllers/CabinController.java
@@ -4,10 +4,8 @@ import com.sytoss.edu2021.models.Cabin;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 
 @Controller
 public class CabinController {
@@ -15,25 +13,22 @@ public class CabinController {
     private final Cabin cabin = new Cabin(-3, 12);
 
     @GetMapping("/")
-    public String writeEngine(Model model, @RequestParam(name = "newFloor",required = false) Integer floor) {
+    public String cabinInterfaceLogic(Model model) {
 
         Arrays.sort(cabin.getFloorButtons(), Collections.reverseOrder());
         model.addAttribute("title", "Cabin Interface");
         model.addAttribute("buttonsList", cabin.getFloorButtons());
-
-
-        if(floor != null){
-
-            System.out.println("Floor from form:  = " + floor);
-            System.out.println("Floor from getDirection:  = " + cabin.getDirection());
-
-            cabin.addFloorToStop(floor);
-            model.addAttribute("dir", cabin.getDirection());
-        }
-
-
+        model.addAttribute("dir", cabin.getDirection());
+        model.addAttribute("stopsList", cabin.getQueueOfFloors());
 
         return "cabin";
+    }
+
+
+    @PostMapping("/addFloor")
+    public String processForm(@RequestParam(name = "newFloor") Integer floor) {
+        cabin.addFloorToStop(floor);
+        return "redirect:/";
     }
 
 }

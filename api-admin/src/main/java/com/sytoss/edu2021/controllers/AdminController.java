7ab86356d72d4a3459c33ede93e4236c3a003160
DarkMinimum <danil.grechishkin@gmail.com>
2021-06-17 13:33:45 +0300

    Add liquibase

--- api-admin/src/main/java/com/sytoss/edu2021/controllers/AdminController.java
+++ api-admin/src/main/java/com/sytoss/edu2021/controllers/AdminController.java
@@ -1,14 +1,31 @@
 package com.sytoss.edu2021.controllers;
 
+
+import com.sytoss.edu2021.models.Building;
+import com.sytoss.edu2021.models.User;
+import com.sytoss.edu2021.repo.BuildingRepository;
+import com.sytoss.edu2021.repo.CabinRepository;
+import com.sytoss.edu2021.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AdminController {
-    @GetMapping("/")
+
+    @Autowired
+    BuildingRepository buildingRepository;
+    @Autowired
+    CabinRepository cabinRepository;
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/admin")
     @ResponseBody
-    public String hello(){
-        return "Hello, World";
+    public Iterable<User> writeEngine() {
+
+        return userRepository.findAll();
     }
+
 }

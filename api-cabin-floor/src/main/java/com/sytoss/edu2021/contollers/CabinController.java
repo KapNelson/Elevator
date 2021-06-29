af8bd7d170933cde1ac01ecf3e877f6e07a59964
DarkMinimum <danil.grechishkin@gmail.com>
2021-06-29 13:28:10 +0300

    Rest queries

--- api-cabin-floor/src/main/java/com/sytoss/edu2021/contollers/CabinController.java
+++ api-cabin-floor/src/main/java/com/sytoss/edu2021/contollers/CabinController.java
@@ -1,38 +1,40 @@
 package com.sytoss.edu2021.contollers;
 
-//import com.sytoss.edu2021.models.Cabin;
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers;
+import com.sytoss.edu2021.models.Cabin;
+import lombok.Builder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.Arrays;
 import java.util.Collections;
 
-
 @Controller
 public class CabinController {
 
-/*    private final Cabin cabin = new Cabin(-3, 12);
+    private final Cabin cabin = new Cabin(-3, 12);
+
 
     @GetMapping("/")
-    public String cabinInterfaceLogic(Model model) {
+    public String cabinInterfaceLogic(Model model, @RequestParam(name = "curFloor", required = true,defaultValue="-13") Integer floor) {
 
+        cabin.setCurrentFloor(floor);
         Arrays.sort(cabin.getFloorButtons(), Collections.reverseOrder());
         model.addAttribute("title", "Cabin Interface");
         model.addAttribute("buttonsList", cabin.getFloorButtons());
         model.addAttribute("dir", cabin.getRoute().getDirection());
         model.addAttribute("stopsList", cabin.getRoute().getQueueOfFloors());
-
         return "cabin";
     }
-
 
     @PostMapping("/addFloor")
     public String processForm(@RequestParam(name = "newFloor") Integer floor) {
         cabin.addFloorToStop(floor);
-        return "redirect:/";
-    }*/
+
+        return "redirect:";
+    }
 
 }
--- api-engine/src/main/java/com/sytoss/edu2021/controllers/EngineController.java
+++ api-engine/src/main/java/com/sytoss/edu2021/controllers/EngineController.java
@@ -3,8 +3,9 @@ package com.sytoss.edu2021.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class EngineController {
     @GetMapping("/engine")
     @ResponseBody
